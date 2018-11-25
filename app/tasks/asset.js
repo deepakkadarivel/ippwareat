31fc781004cd4fc6943adb51f36ad5b839a3e0e4
Deepak <d.kadarivel@relay42.com>
2018-11-25 18:00:51 +0100

    Asset header API

--- app/tasks/asset.js
+++ app/tasks/asset.js
@@ -6,10 +6,81 @@ const BASE_URL = process.env.BASE_URL;
 
 const url = `${BASE_URL}${constants.url.ASSET}`;
 
+const parseAsset = asset => {
+  return {
+    header: [
+      {
+        label: 'Chrome_Entity',
+        id: 'entityName',
+        name: 'entityName',
+        maxlength: '50',
+        placeholder: 'Entity',
+        type: 'text',
+        value: asset.entityName,
+        readOnly: true,
+      },
+      {
+        label: 'Chrome_View',
+        id: 'viewName',
+        name: 'viewName',
+        maxlength: '50',
+        placeholder: 'View',
+        type: 'text',
+        value: asset.viewName,
+        readOnly: true,
+      },
+      {
+        label: 'Asset Request #',
+        id: 'assetRequestNo',
+        name: 'assetRequestNo',
+        maxlength: '50',
+        placeholder: 'Asset Request #',
+        type: 'text',
+        value: asset.assetRequestNo,
+        readOnly: true,
+      },
+      {
+        label: 'Workflow',
+        id: 'workflowId',
+        name: 'workflowId',
+        type: 'select',
+        value: asset.workflowId,
+        readOnly: true,
+        options: Object.values(asset.workflowList).map(x => {return {value: x.workflowId, label: x.workflowName}}),
+      },
+      {
+        label: 'WareHouse',
+        id: 'wareHouse',
+        name: 'wareHouse',
+        type: 'select',
+        value: asset.wareHouse,
+        readOnly: true,
+        options: Object.values(asset.wareHouseList).map(x => {return {value: x.wareHouseMasterId, label: x.value}}),
+      },
+      {
+        label: 'Need Date',
+        id: 'needDate',
+        name: 'needDate',
+        type: 'date',
+        value: asset.needDate,
+        readOnly: true,
+      },
+      {
+        label: 'Return Date',
+        id: 'returnDate',
+        name: 'returnDate',
+        type: 'date',
+        value: asset.returnDate,
+        readOnly: true,
+      },
+    ]
+  }
+};
+
 const getAsset = async (req, res, next) => {
   logger.info(`${req.originalUrl} - ${req.method} - ${req.ip}`);
   try {
-    const { cookie, loadBalancer, payload } = req.body;
+    const {cookie, loadBalancer, payload} = req.body;
 
     const config = {
       headers: {
@@ -26,7 +97,9 @@ const getAsset = async (req, res, next) => {
       err.status = 401;
       next(err);
     } else {
-      res.status(200).json(response.data);
+      const asset = await parseAsset(response.data);
+      // logger.info(JSON.stringify(asset));
+      res.status(200).json(asset);
     }
   } catch (err) {
     if (err.toString() === constants.STATUS_401) {
