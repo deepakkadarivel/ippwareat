8715ee9d613d6744f6c16e3a1de891b0aca468f0
Deepak <d.kadarivel@relay42.com>
2018-11-25 18:37:49 +0100

    Invoice header api

--- app/tasks/invoice.js
+++ app/tasks/invoice.js
@@ -6,6 +6,102 @@ const BASE_URL = process.env.BASE_URL;
 
 const url = `${BASE_URL}${constants.url.INVOICE}`;
 
+const parseInvoice = invoice => {
+  return {
+    header: [
+      {
+        label: 'Chrome_Entity',
+        id: 'entityName',
+        name: 'entityName',
+        maxlength: '50',
+        placeholder: 'Entity',
+        type: 'text',
+        value: invoice.entityName,
+        readOnly: true,
+      },
+      {
+        label: 'PO Type',
+        id: 'poFrom',
+        name: 'poFrom',
+        maxlength: '50',
+        placeholder: 'PO',
+        type: 'text',
+        value: 'PO',
+        readOnly: true,
+      },
+      {
+        label: 'Invoice #',
+        id: 'invoiceNo',
+        name: 'invoiceNo',
+        maxlength: '50',
+        placeholder: 'Invoice #',
+        type: 'text',
+        value: invoice.invoice.invoiceNo,
+        readOnly: true,
+      },
+      {
+        label: 'Invoice Date',
+        id: 'invoiceDate',
+        name: 'invoiceDate',
+        maxlength: '50',
+        placeholder: 'Invoice Date',
+        type: 'text',
+        value: invoice.invoiceDate,
+        readOnly: true,
+      },
+      {
+        label: 'Workflow',
+        id: 'workflowName',
+        name: 'workflowName',
+        maxlength: '50',
+        placeholder: 'Workflow',
+        type: 'text',
+        value: invoice.workflowName,
+        readOnly: true,
+      },
+      {
+        label: 'Supplier',
+        id: 'supplierName',
+        name: 'supplierName',
+        maxlength: '50',
+        placeholder: 'Supplier',
+        type: 'text',
+        value: invoice.supplierName,
+        readOnly: true,
+      },
+      {
+        label: 'PO',
+        id: 'poNo',
+        name: 'poNo',
+        maxlength: '50',
+        placeholder: 'PO',
+        type: 'text',
+        value: invoice.invoice.invoiceLineItems[0].poNo,
+        readOnly: true,
+      },
+      {
+        label: 'Payment Terms(Days)',
+        id: 'paymentDays',
+        name: 'paymentDays',
+        maxlength: '50',
+        placeholder: 'Payment Terms(Days)',
+        type: 'text',
+        value: invoice.paymentDays,
+        readOnly: true,
+      },
+      {
+        label: 'Credit Notes',
+        id: 'creditNotes',
+        name: 'creditNotes',
+        type: 'select',
+        value: invoice.creditNotes,
+        readOnly: true,
+        options: [],
+      },
+    ]
+  }
+};
+
 const getInvoice = async (req, res, next) => {
   logger.info(`${req.originalUrl} - ${req.method} - ${req.ip}`);
   try {
@@ -26,7 +122,9 @@ const getInvoice = async (req, res, next) => {
       err.status = 401;
       next(err);
     } else {
-      res.status(200).json(response.data);
+      const invoice = await parseInvoice(response.data);
+      // logger.info(JSON.stringify(invoice));
+      res.status(200).json(invoice);
     }
   } catch (err) {
     if (err.toString() === constants.STATUS_401) {
